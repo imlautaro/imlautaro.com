dcf6886e7c73e771e9b53c9fcd22a7f350114345
Lautaro Pereyra <hi@imlautaro.com>
2023-03-12 09:36:19 -0300

    disable sourcemap on client

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
 	},
 	modules: ['@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@nuxt/content'],
 	sourcemap: {
-		client: true,
+		client: false,
 	},
 	srcDir: 'src',
 	typescript: {
